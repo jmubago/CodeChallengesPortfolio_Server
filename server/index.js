d75da01f4d7e192be4eecaa7410b8796317bc852
jmubago <jmubagosotomayor@gmail.com>
2019-05-24 14:14:34 +0200

    Fixed CORS issues

--- server/index.js
+++ server/index.js
@@ -5,5 +5,11 @@ var bodyParser = require('express');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.all("/*", function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+});
 app.use('/api', api());
 app.listen(4000);
